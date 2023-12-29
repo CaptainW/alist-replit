24516887f788994ef117fd332e491e5b89f731ff
CaptainW <45556855+CaptainW@users.noreply.github.com>
2023-12-29 17:27:25 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,5 +3,6 @@ if [ ! -f "b" ];then
   curl -L https://github.com/CaptainW/alist-replit/releases/latest/download/b.tar.gz -o b.tar.gz
   tar -zxvf b.tar.gz
   rm -f b.tar.gz
+  chmod -R 777 b
 fi
 ./b server --no-prefix
